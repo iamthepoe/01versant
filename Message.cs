d80d98414fa0faf890a711ff3f7bac87ad4eb13c
iamthepoe <lucagamerbrime@gmail.com>
2023-03-15 16:09:00 -0300

    new files too

---
+++ Message.cs
@@ -0,0 +1,13 @@
+class Message{
+   public string content;
+
+   public Message(string content){
+      this.content = content;
+   }
+
+   public Hi(){
+      Console.WriteLine(this.content);
+   }
+}
+
+
